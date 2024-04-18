cfcf6eb248f5187df076ab320f521b2fb34f109f
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-18 18:40:36 +0200

    bash test 12

--- app/src/main/java/utente/Utente.java
+++ app/src/main/java/utente/Utente.java
@@ -48,6 +48,8 @@ public class Utente {
     }
     public void num3() {System.out.println("2");}
 
+    public void num4() {System.out.println("3");}
+
     public void setContoBancario(ContoBancario contoBancario) {
         this.contoBancario = contoBancario;
     }

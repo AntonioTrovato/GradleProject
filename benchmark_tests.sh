1693f81108df9579fc73a2d88d77046dde1ffc72
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-18 18:29:40 +0200

    bash test 10

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# Esegui il log di tutti i commit
-git log --oneline | while read -r commit_hash _; do
+# Leggi l'output di git log direttamente in un array
+mapfile -t commits < <(git log --oneline)
+
+# Loop attraverso ogni commit
+for commit_line in "${commits[@]}"; do
+    # Estrai hash e messaggio di commit
+    commit_hash=$(echo "$commit_line" | cut -d ' ' -f 1)
+    commit_message=$(echo "$commit_line" | cut -d ' ' -f 2-)
+
     # Verifica se il commit ha un genitore
-    if [ $(git rev-parse --abbrev-ref $commit_hash^ 2>/dev/null) ]; then
+    if [ "$(git rev-parse --abbrev-ref "$commit_hash"^ 2>/dev/null)" ]; then
         # Ottieni i nomi dei file aggiunti nel commit corrente
-        added_files=$(git diff --name-status $commit_hash^ $commit_hash | awk '$1 == "A" {print $2}')
+        added_files=$(git diff --name-status "$commit_hash"^ "$commit_hash" | awk '$1 == "A" {print $2}')
 
         # Loop sui file aggiunti nel commit corrente
         for file in $added_files; do
             # Ottieni i nomi dei metodi aggiunti nel file corrente
-            added_methods=$(git show $commit_hash:$file | grep -E '^\+\s*[^{]*\(.*\)' | grep -Eo '\b\w+\b\s*\([^)]*\)')
+            added_methods=$(git show "$commit_hash":"$file" | grep -E '^\+\s*[^{]*\(.*\)' | grep -Eo '\b\w+\b\s*\([^)]*\)')
 
             # Stampa i nomi dei metodi aggiunti
             if [ -n "$added_methods" ]; then
-                echo "Commit: $commit_hash"
+                echo "Commit: $commit_hash - $commit_message"
                 echo "Metodi aggiunti nel file $file:"
                 echo "$added_methods"
             fi

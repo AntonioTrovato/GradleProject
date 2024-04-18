f0501d4f967a09b4d88c8083360024be1fedcb49
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-18 18:59:37 +0200

    bash test 17

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ottieni l'hash del primo commit nella cronologia
+first_commit=$(git rev-list --max-parents=0 HEAD)
+
 # Ottieni la lista dei commit
 commit_list=$(git rev-list --reverse HEAD)
 echo "Lista dei commit:"
@@ -8,11 +11,12 @@ echo "$commit_list"
 # Loop attraverso ogni commit
 for commit_hash in $commit_list; do
     echo "Elaborazione del commit: $commit_hash"
-    # Verifica se il commit ha un genitore
-    parent_commit=$(git show --no-patch --format="%P" "$commit_hash" | awk '{print $1}')
-    if [ -n "$parent_commit" ]; then
+    # Verifica se il commit è il primo nella cronologia
+    if [ "$commit_hash" == "$first_commit" ]; then
+        echo "Il commit $commit_hash è il primo nella cronologia e non ha un genitore."
+    else
         # Ottieni i nomi dei file aggiunti nel commit corrente
-        added_files=$(git diff --name-status "$parent_commit".."$commit_hash" | awk '$1 == "A" {print $2}')
+        added_files=$(git diff --name-status "$commit_hash"^.."$commit_hash" | awk '$1 == "A" {print $2}')
 
         # Loop sui file aggiunti nel commit corrente
         for file in $added_files; do
@@ -26,7 +30,5 @@ for commit_hash in $commit_list; do
                 echo "$added_methods"
             fi
         done
-    else
-        echo "Il commit $commit_hash è il primo nella cronologia e non ha un genitore."
     fi
 done

cdd910e1fa344eb6bde95b7f2eaf09c1f50c240d
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-22 16:10:18 +0200

    trying actions 6

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git diff
+# Leggi gli hash dei due commit più recenti utilizzando git log
+commit_corrente=$(git log --format="%H" -n 1)
+commit_precedente=$(git log --format="%H" -n 2 | tail -n 1)
+
+# Esegui la diff tra i due commit
+git diff $commit_precedente $commit_corrente

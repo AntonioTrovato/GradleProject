a91e550e341f8dd667cff0e94c26a8543d7d38ed
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-22 16:19:21 +0200

    trying actions 10

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 # Leggi l'hash del commit corrente
-commit_corrente=$(git rev-parse HEAD)
-
-# Leggi l'hash del commit precedente
-commit_precedente=$(git log --format="%H" -n 2 | tail -n 1)
-
-# Stampare gli hash dei due commit
-echo "Hash del commit corrente: $commit_corrente"
-echo "Hash del commit precedente: $commit_precedente"
-
-# Esegui la diff tra i due commit
-git diff $commit_precedente $commit_corrente
+git log

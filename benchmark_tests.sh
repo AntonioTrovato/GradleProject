3e3e9c7e8a91cf3e2297ba97f51333b4328cec75
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-22 15:40:08 +0200

    trying actions 3

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-commit_hash=$(git rev-parse HEAD)
-
-# Ottieni l'hash del commit precedente
-parent_commit_hash=$(git rev-parse HEAD^)
-
-# Esegui la diff tra i due commit
-git diff $parent_commit_hash $commit_hash
+git diff 'HEAD^'

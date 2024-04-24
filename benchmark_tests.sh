3eff4f264d66da748fb2e4a6cc2383d597ff529a
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-24 18:01:07 +0200

    trying dividing diff string 8

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -7,8 +7,6 @@ commit_precedente=$(git log --format="%H" -n 2 | tail -n 1)
 # Esegui la diff tra i due commit
 git_diff=$(git diff $commit_precedente $commit_corrente)
 
-echo "$git_diff"
-
 # Utilizza awk per separare la stringa in blocchi
 echo "$input_string" | grep -E '^diff --git ' | while IFS= read -r block; do
     echo "ecco:"

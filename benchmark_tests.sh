e1e60ba49a27b14ed91f81b462e2ea0278375601
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-25 13:07:14 +0200

    trying getting deleted methods 17

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -56,7 +56,7 @@ for commit_block in "${commit_blocks[@]}"; do
     while IFS= read -r line; do
       echo "LINE:"
       echo $line
-      if [[ $line =~ ^-\s*(public|protected|private|static|final|abstract|synchronized)\s.* ]]; then
+      if [[ $line =~ ^-\s*public\s.* ]]; then
           echo "ciao"
           #method_name="${BASH_REMATCH[3]}"
           #echo "$class_name.$method_name"

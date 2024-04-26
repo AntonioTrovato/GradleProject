969535874f431261306c77f0daed618202525170
AntonioTrovato <72383399+AntonioTrovato@users.noreply.github.com>
2024-04-26 20:08:36 +0200

    trying movng benchmark classes to prepare the generation 7

--- benchmark_tests.sh
+++ benchmark_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chmod +x "$0"
-mkdir -p "./ciao"
+mkdir -p ./ciao
 # Leggi gli hash dei due commit più recenti utilizzando git log
 commit_corrente=$(git log --format="%H" -n 1)
 commit_precedente=$(git log --format="%H" -n 2 | tail -n 1)

c5a4e2bf6deed7c6e7e8bb314f4fd9fcc439e03c
Antonio Trovato <atrovato@unisa.it>
2024-10-12 12:22:28 +0200

    Making tests

---
+++ app/src/main/java/utente/Utente2.java
@@ -0,0 +1,18 @@
+package utente;
+
+public class Utente2 {
+
+    public Utente2(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String nome;
+}
